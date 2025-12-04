efcb8f21b8cdc449f4ba45f53e355cdac3526892
Sara Nafaryeh <84543239+snafarye@users.noreply.github.com>
2025-12-03 18:37:32 -0600

    Add files via upload

---
+++ SQL Basic Training_Music_12.02.2025.sql
@@ -0,0 +1,13 @@
+/*
+
+CREATED By: Sara Nafaryeh
+CREATED DATE: 12/01/2025
+DESCRIPTION: this report will dysply properly the customers first and last names wit their email addresses
+
+*/
+SELECT
+	FirstName, 
+	LastName, 
+	Email
+FROM
+	Customer
