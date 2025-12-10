83bccaac97a5373bb50ff4c37fa0c2f06d42b882
Sara Nafaryeh <84543239+snafarye@users.noreply.github.com>
2025-12-10 09:34:06 -0600

    Refactor SQL query with aliases and ordering
    Updated SQL query to include aliases for columns and modified the order by clause.

--- SQL Basic Training_Music_12.02.2025.sql
+++ SQL Basic Training_Music_12.02.2025.sql
@@ -5,9 +5,19 @@ CREATED DATE: 12/01/2025
 DESCRIPTION: this report will dysply properly the customers first and last names wit their email addresses
 
 */
+
 SELECT
-	FirstName, 
-	LastName, 
-	Email
+	FirstName as [Customer First Name], -- add an aliese/ renaming te column Name
+	LastName as "Customer Last Name", 
+	Email as EMAIL -- no [] or "" since its just one word
 FROM
 	Customer
+
+ORDER by
+	 --LastName ASC, FirstName DESC
+	 FirstName, LastName DESC
+	-- LastName DESC
+	
+LIMIT --have to put it after order by
+	10
+
